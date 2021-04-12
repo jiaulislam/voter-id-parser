272d0e13988f1e3f1d97dd864eede1a7890787de
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-04-12 23:14:39 +0600

    modern

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
+from os import name
 from pages.basepage import BasePage
 from selenium import webdriver
 from pages.searchresult import SearchResult
 import time
+import json
 
 URL = "C:\\Users\\Jibon\\Desktop\\JJ\\Verify By Nid.htm"
 
-def parse(driver):
+def parse(driver=None):
     page = SearchResult(driver)
-    nameBangla = page.parse_name_bangla()
-    nameEnglish = page.parse_name_english()
-    fatherName = page.parse_father_name()
-    motherName = page.parse_mother_name()
-    return [nameBangla, nameEnglish, fatherName, motherName]
+    basic_info = page.parse_basic_info()
+    present_address = page.parse_present_address()
+    permenent_address = page.parse_permenent_address()
+    basic_info['present_address'] = present_address
+    basic_info['permenent_address'] = permenent_address
+    return basic_info
+    
     
 if __name__=="__main__":
     driver = webdriver.Firefox(executable_path="C:\\geckodriver-v0.29.1-win64\\geckodriver.exe")
     driver.get(URL)
-    names = parse(driver)
-    time.sleep(4)
-    with open("jst.json", "w", encoding="utf-8") as e:
-        for name in names:
-            e.write(name+"\n")
+    for i , v in parse(driver).items():
+        print(i , "-> ", v)
--- pages/searchresult.py
+++ pages/searchresult.py
@@ -3,50 +3,57 @@ from selenium.webdriver.common.by import By
 
 class SearchResult(BasePage):
     # Names, Occupation, Blood-Group, National ID, Pin
-    NAME_BANGLA = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[2]")
-    NAME_ENGLISH= (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[4]")
-    FATHER_NAME = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[8]")
-    MOTHER_NAME = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[10]")
-    SPOUSE_NAME = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[12]")
-    DOB         = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[6]")
-    OCCUPATION  = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[14]")
-    BLOOD_GROUP = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[20]")
-    NATIONAL_ID = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[22]")
-    PIN         = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[24]")
+    BASIC_INFO = {
+        "NAME_BANGLA" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[2]"),
+        "NAME_ENGLISH": (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[4]"),
+        "FATHER_NAME" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[8]"),
+        "MOTHER_NAME" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[10]"),
+        "SPOUSE_NAME" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[12]"),
+        "DOB"         : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[6]"),
+        "OCCUPATION"  : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[14]"),
+        "BLOOD_GROUP" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[20]"),
+        "NATIONAL_ID" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[22]"),
+        "PIN"         : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[24]")
+    }
+
 
     # PRESENT ADDRESS SECTION
-    PRE_DIVISION            = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[2]")
-    PRE_DISTRICT            = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[4]")
-    PRE_RMO                 = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[6]")
-    PRE_CITY_CORP           = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[8]")
-    PRE_UPOZILA             = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[10]")
-    PRE_UNION               = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[12]")
-    PRE_MOHOLLA             = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[14]")
-    PRE_ADDITIONAL_MOHOLLA  = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[16]")
-    PRE_WARD_UNION_PARISHAD = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[18]")
-    PRE_VILLAGE             = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[20]")
-    PRE_ADDITIONAL_ROAD     = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[22]")
-    PRE_HOME_HOLDING        = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[24]")
-    PRE_POST_OFFICE         = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[26]")
-    PRE_POSTAL_CODE         = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[28]")
-    PRE_REGION              = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[30]")
+    PRESENT_ADDRESS = {
+        "PRE_DIVISION"            : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[2]"),
+        "PRE_DISTRICT"            : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[4]"),
+        "PRE_RMO"                 : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[6]"),
+        "PRE_CITY_CORP"           : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[8]"),
+        "PRE_UPOZILA"             : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[10]"),
+        "PRE_UNION"               : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[12]"),
+        "PRE_MOHOLLA"             : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[14]"),
+        "PRE_ADDITIONAL_MOHOLLA"  : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[16]"),
+        "PRE_WARD_UNION_PARISHAD" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[18]"),
+        "PRE_VILLAGE"             : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[20]"),
+        "PRE_ADDITIONAL_ROAD"     : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[22]"),
+        "PRE_HOME_HOLDING"        : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[24]"),
+        "PRE_POST_OFFICE"         : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[26]"),
+        "PRE_POSTAL_CODE"         : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[28]"),
+        "PRE_REGION"              : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[16]/div/div[30]")
+    }
 
     #PERMENENT ADDRESS
-    PERME_DIVISION            = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[2]")
-    PERME_DISTRICT            = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[4]")
-    PERME_RMO                 = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[6]")
-    PERME_CITY_CORP           = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[8]")
-    PERME_UPOZILA             = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[10]")
-    PERME_UNION               = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[12]")
-    PERME_MOHOLLA             = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[14]")
-    PERME_ADDITIONAL_MOHOLLA  = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[16]")
-    PERME_WARD_UNION_PARISHAD = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[18]")
-    PERME_VILLAGE             = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[20]")
-    PERME_ADDITIONAL_ROAD     = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[22]")
-    PERME_HOME_HOLDING        = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[24]")
-    PERME_POST_OFFICE         = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[26]")
-    PERME_POSTAL_CODE         = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[28]")
-    PERME_REGION              = (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[30]")
+    PERMENENT_ADDRESS = {
+        "PERME_DIVISION"            : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[2]"),
+        "PERME_DISTRICT"            : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[4]"),
+        "PERME_RMO"                 : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[6]"),
+        "PERME_CITY_CORP"           : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[8]"),
+        "PERME_UPOZILA"             : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[10]"),
+        "PERME_UNION"               : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[12]"),
+        "PERME_MOHOLLA"             : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[14]"),
+        "PERME_ADDITIONAL_MOHOLLA"  : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[16]"),
+        "PERME_WARD_UNION_PARISHAD" : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[18]"),
+        "PERME_VILLAGE"             : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[20]"),
+        "PERME_ADDITIONAL_ROAD"     : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[22]"),
+        "PERME_HOME_HOLDING"        : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[24]"),
+        "PERME_POST_OFFICE"         : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[26]"),
+        "PERME_POSTAL_CODE"         : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[28]"),
+        "PERME_REGION"              : (By.XPATH, "//*[@id='result-container']/div[3]/div[1]/div/div[18]/div/div[30]")
+    }
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -57,35 +64,41 @@ class SearchResult(BasePage):
             return True
         return False
 
-    def _parse_name_bangla(self):
-        val = self.get_element_text(self.NAME_BANGLA)
-        if self.__is_blank(val):
-            return None
-        return val
+    def _parse_basic_info(self):
+        basic_info = {}
+        for key, *selector in self.BASIC_INFO.items():
+            parsed_data =self.get_element_text(*selector)
+            if self.__is_blank(parsed_data):
+                parsed_data = None
+            basic_info[key] = parsed_data
+        return basic_info
 
-    def _parse_name_english(self):
-        val = self.get_element_text(self.NAME_ENGLISH)
-        if self.__is_blank(val):
-            return None
-        return val
+    def _parse_present_address(self):
+        present_address_data = []
+        for header, *selector in self.PRESENT_ADDRESS.items():
+            parsed_data = self.get_element_text(*selector)
+            if self.__is_blank(parsed_data):
+                parsed_data = None
+            present_address_data.append({header : parsed_data})
 
-    def _parse_father_name(self):
-        val = self.get_element_text(self.FATHER_NAME)
-        if self.__is_blank(val):
-            return None
-        return val
+        return present_address_data
 
-    def _parse_mother_name(self):
-        val = self.get_element_text(self.MOTHER_NAME)
-        if self.__is_blank(val):
-            return None
-        return val
+    def _parse_permenent_address(self):
+        permenent_address_data = []
+        for header, *selector in self.PERMENENT_ADDRESS.items():
+            parsed_data = self.get_element_text(*selector)
+            if self.__is_blank(parsed_data):
+                parsed_data = None
+            permenent_address_data.append({header : parsed_data})
+        return permenent_address_data
 
     def parse_basic_info(self):
-        pass
+        return self._parse_basic_info()
+
 
     def parse_present_address(self):
-        pass
+        return self._parse_present_address()
 
     def parse_permenent_address(self):
-        pass
+        return self._parse_permenent_address()
+

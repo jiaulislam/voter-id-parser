3ccccbd075e693d552736bfdaf2155bb99427474
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-04-20 12:26:32 +0600

    Refactored View

--- api/voter_id_parser/views.py
+++ api/voter_id_parser/views.py
@@ -6,23 +6,19 @@ from actions import SearchAction
 from .forms import RequestForm
 
 
-def __make_query(webpage, nid, dob):
-    return SearchAction(webpage, nid, dob)
-
-
 def search(request):
     if request.method == 'POST':
         form = RequestForm(request.POST)
         if form.is_valid():
             nid = form.cleaned_data['nid_number']
             dob = form.cleaned_data['date_of_birth']
-            json = __make_query(settings.WEBDRIVER_WINDOW, nid, dob)
+            json = SearchAction(settings.WEBDRIVER_WINDOW, nid, dob)
             return HttpResponse(json, content_type="application/json")
     else:
         if request.GET.get('nid') and request.GET.get('dob'):
             nid = request.GET.get('nid')
             dob = request.GET.get('dob')
-            json = __make_query(settings.WEBDRIVER_WINDOW, nid, dob)
+            json = SearchAction(settings.WEBDRIVER_WINDOW, nid, dob)
             return HttpResponse(json, content_type="application/json")
         else:
             form = RequestForm()

d1fa733c22fb86aa8d5aaaa420582ae82b46494e
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-04-20 14:12:43 +0600

    refactoring

--- api/voter_id_parser/forms.py
+++ api/voter_id_parser/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
+
 class RequestForm(forms.Form):
-    nid_number = forms.CharField(max_length=17, label='NID', 
-        widget=forms.TextInput(attrs={'class':'form-control'}))
+    nid_number = forms.CharField(max_length=17, label='NID',
+                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
     date_of_birth = forms.CharField(max_length=10, label="Date of Birth",
-        widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'YYYY-MM-DD'}))
-    
+                                    widget=forms.TextInput(
+                                        attrs={'class': 'form-control', 'placeholder': 'YYYY-MM-DD'}))

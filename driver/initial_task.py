942dbf6b767b531f2af257258cc5f952fe82c80c
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-04-22 22:36:15 +0600

    module created

---
+++ driver/initial_task.py
@@ -0,0 +1,25 @@
+from browser import get_browser_window
+from static_data import STATIC_DATA
+from actions import (
+    OpenURL,
+    DoLogin,
+    GotoNIDVerification
+)
+
+"""
+It's a simple module for the first time app
+runs then a a remote connection should set
+and get the chrome driver the do the one time
+task.
+"""
+
+
+def initial_task():
+    driver = get_browser_window()  # Connect to remote Chrome
+    OpenURL(driver, STATIC_DATA.get('URL'))  # Open the Provided URL
+    DoLogin(driver)  # Login to the website
+    GotoNIDVerification(driver)  # Goto the NID Verification Section
+
+
+if __name__ == "__main__":
+    initial_task()
